536a7b9254e002df546a5e300691cbec2144f6b0
Quoc Tri Do <qdo@1-C-22.42heilbronn.de>
2024-05-21 09:38:32 +0200

    contact.cpp

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,33 +6,30 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 07:50:18 by qdo               #+#    #+#             */
-/*   Updated: 2024/05/20 08:20:24 by qdo              ###   ########.fr       */
+/*   Updated: 2024/05/21 09:07:21 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
-#include <iostream>
+# include "Contact.hpp"
 
-class Contact
+class PhoneBook
 {
 private:
-	std::string	firstName;
-	std::string	lastName;
-	std::string	nickName;
-	std::string	phoneNumber;
-	std::string	darkestSecret;
-
-public:
-	Contact(std::string first, std::string last, std::string nick, std::string phone, std::string secret):
-		firstName(first), lastName(last), nickName(nick), phoneNumber(phone), darkestSecret(secret) {}
+		Contact		_contacts[8];
+		int			_index;
+		int			_full;
 	
-	std::string	getFrist() 	{return (firstName);}
-	std::string getLast() {return (lastName);}
-	std::string getNick() {return (nickName);}
-	std::string getPhone() {return (phoneNumber);}
-	std::string getSecret() {return (darkestSecret);}
-}
+public:
+		PhoneBook(void);
+		~PhoneBook(void);
+		bool	add(void);
+		void	search(void);
+};
+
+int			countWords(const std::string& str);
+std::string actualString(std::string str);
 
 #endif
